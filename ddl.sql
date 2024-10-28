ad6306bffa7f9ebbe14c748c5016d82928f6fe06
Alex molin <alexmolin@Alexs-MacBook-Pro.local>
2024-10-28 13:55:19 +0100

    improved ddl sustainability by declaring scheme

--- ddl.sql
+++ ddl.sql
@@ -1,5 +1,6 @@
-DROP TABLE IF EXISTS customers;
-CREATE TABLE customers 
+
+DROP TABLE IF EXISTS project1.customers CASCADE;
+CREATE TABLE project1.customers 
 (
     customer_id SERIAL PRIMARY KEY,
     name varchar(255) NOT NULL,
@@ -8,91 +9,104 @@ CREATE TABLE customers
     address varchar(255) NOT NULL
 );
 
-DROP TABLE IF EXISTS aircrafts;
-CREATE TABLE aircrafts (
+DROP TABLE IF EXISTS project1.aircrafts CASCADE;
+CREATE TABLE project1.aircrafts (
     aircraft_registration_number SERIAL PRIMARY KEY,
     aircraft_type varchar(255) NOT NULL,
     aircraft_company varchar(255) NOT NULL,
     aircraft_capacity int NOT NULL
 );   
 
-DROP TABLE IF EXISTS airports;
-CREATE TABLE airports (
+DROP TABLE IF EXISTS project1.airports CASCADE;
+CREATE TABLE project1.airports (
     airport_id varchar(3) NOT NULL PRIMARY KEY,
     airport_name varchar(255) NOT NULL,
     airport_city varchar(255) NOT NULL,
     airport_country varchar(255) NOT NULL
 );   
 
-DROP TABLE IF EXISTS flights;
-CREATE TABLE flights (
+DROP TABLE IF EXISTS project1.flights CASCADE;
+CREATE TABLE project1.flights (
     flight_number varchar(6) NOT NULL PRIMARY KEY,
     origin varchar(3) NOT NULL,
     destination varchar(3) NOT NULL,
-    date DATE NOT NULL,
-    time TIMESTAMP NOT NULL,
+    airline_id INT NOT NULL,
+    flight_length INTERVAL NOT NULL,
     CONSTRAINT fk_origin_airport
-        FOREIGN KEY (origin) REFERENCES airports (airport_id),
+        FOREIGN KEY (origin) REFERENCES project1.airports (airport_id),
     CONSTRAINT fk_destination_airport
-        FOREIGN KEY (destination) REFERENCES airports (airport_id)
+        FOREIGN KEY (destination) REFERENCES project1.airports (airport_id),
+    CONSTRAINT fk_airline_id
+        FOREIGN KEY (airline_id) REFERENCES project1.airlines (airline_id)
 ); 
 
-DROP TABLE IF EXISTS airlines;
-CREATE TABLE airlines (
+DROP TABLE IF EXISTS project1.airlines CASCADE;
+CREATE TABLE project1.airlines (
     airline_id SERIAL PRIMARY KEY,
     airline_name varchar(255) NOT NULL
 ); 
 
-DROP TABLE IF EXISTS problems;
-CREATE TABLE problems (
+DROP TABLE IF EXISTS project1.problems CASCADE;
+CREATE TABLE project1.problems (
     problem_id SERIAL PRIMARY KEY,
     problem_type varchar(255) NOT NULL
 ); 
 
-DROP TABLE IF EXISTS flight_statuses;
-CREATE TABLE flight_statuses (
+DROP TABLE IF EXISTS project1.flight_statuses CASCADE;
+CREATE TABLE project1.flight_statuses (
     flight_status_id SERIAL PRIMARY KEY,
     flight_status_type varchar(255) NOT NULL
 ); 
 
-DROP TABLE IF EXISTS flight_data;
-CREATE TABLE flight_data (
+DROP TABLE IF EXISTS project1.flight_data CASCADE;
+CREATE TABLE project1.flight_data (
     flight_id SERIAL PRIMARY KEY,
     flight_number varchar(6) NOT NULL,
     aircraft_registration_number INT NOT NULL,
-    airline_id INT NOT NULL,
     flight_status_id INT NOT NULL,
     problem_id INT NOT NULL,
     number_of_passengers int NOT NULL,
     number_of_cabin_crew int NOT NULL,
     number_of_flight_crew int NOT NULL,
     available_seating int NOT NULL,
+    scheduled_departure_date DATE NOT NULL,
+    scheduled_departure_time TIME NOT NULL,
     CONSTRAINT fk_flight_number
-        FOREIGN KEY (flight_number) REFERENCES flights (flight_number),
+        FOREIGN KEY (flight_number) REFERENCES project1.flights (flight_number),
     CONSTRAINT fk_aircraft_registration_number
-        FOREIGN KEY (aircraft_registration_number) REFERENCES aircrafts (aircraft_registration_number),
-    CONSTRAINT fk_airline_id
-        FOREIGN KEY (airline_id) REFERENCES airlines (airline_id),
+        FOREIGN KEY (aircraft_registration_number) REFERENCES project1.aircrafts (aircraft_registration_number),
     CONSTRAINT fk_flight_status_id
-        FOREIGN KEY (flight_status_id) REFERENCES flight_statuses (flight_status_id),
+        FOREIGN KEY (flight_status_id) REFERENCES project1.flight_statuses (flight_status_id),
     CONSTRAINT fk_problem_id
-        FOREIGN KEY (problem_id) REFERENCES problems (problem_id)
+        FOREIGN KEY (problem_id) REFERENCES project1.problems (problem_id)
 ); 
 
-DROP TABLE IF EXISTS seats;
-CREATE TABLE seats 
+
+DROP TABLE IF EXISTS project1.seat_classes CASCADE;
+CREATE TABLE project1.seat_classes 
 (
-    seat_id SERIAL PRIMARY KEY,
-    seat_number varchar(3) NOT NULL, --1a to 40f
+    seat_number varchar(3) NOT NULL PRIMARY KEY, --1a to 40f
     seat_class varchar(33) NOT NULL, --rows 1 - 3 are business and rows 4 - 40 are economy
-    seat_status varchar(33) NOT NULL, --available or occupied
-    flight_id INT NOT NULL -- foreign key
-    CONSTRAINT fk_flight_id
-        FOREIGN KEY (flight_id) REFERENCES flight_data (flight_id)
+    CONSTRAINT fk_seat_number
+        FOREIGN KEY (seat_number) REFERENCES project1.seat_classes (seat_number)
 );
 
-DROP TABLE IF EXISTS bookings;
-CREATE TABLE bookings (
+
+DROP TABLE IF EXISTS project1.seats CASCADE;
+CREATE TABLE project1.seats 
+(
+    seat_id SERIAL PRIMARY KEY,
+    seat_number varchar(3) NOT NULL, -- foreign key
+    seat_status varchar(33) NOT NULL, --available or occupied
+    flight_id INT NOT null, -- foreign_key
+    CONSTRAINT fk_flight_id
+        FOREIGN KEY (flight_id) REFERENCES project1.flight_data (flight_id),
+    CONSTRAINT fk_seat_number
+        FOREIGN KEY (seat_number) REFERENCES project1.seat_classes (seat_number)
+);
+
+DROP TABLE IF EXISTS project1.bookings CASCADE;
+CREATE TABLE project1.bookings (
     booking_id SERIAL PRIMARY KEY,
     flight_id INT NOT NULL,
     customer_id INT NOT NULL,
@@ -100,27 +114,27 @@ CREATE TABLE bookings (
     price NUMERIC(7,2) NOT NULL,
     payment_status BOOLEAN NOT NULL,
     CONSTRAINT fk_flight_id
-        FOREIGN KEY (flight_id) REFERENCES flight_data (flight_id),
+        FOREIGN KEY (flight_id) REFERENCES project1.flight_data (flight_id),
     CONSTRAINT fk_customer_id
-        FOREIGN KEY (customer_id) REFERENCES customers (customer_id),
+        FOREIGN KEY (customer_id) REFERENCES project1.customers (customer_id),
     CONSTRAINT fk_seat_id
-        FOREIGN KEY (seat_id) REFERENCES seats (seat_id)
+        FOREIGN KEY (seat_id) REFERENCES project1.seats (seat_id)
 ); 
 
-DROP TABLE IF EXISTS subsystems;
-CREATE TABLE subsystems (
+DROP TABLE IF EXISTS project1.subsystems CASCADE;
+CREATE TABLE project1.subsystems (
     subsystem_id SERIAL PRIMARY KEY,
     subsystem_type varchar(255) NOT NULL
 ); 
 
-DROP TABLE IF EXISTS maintenance_types;
-CREATE TABLE maintenance_types (
+DROP TABLE IF EXISTS project1.maintenance_types CASCADE;
+CREATE TABLE project1.maintenance_types (
     maintenance_type_id SERIAL PRIMARY KEY,
     maintenance_type_name varchar(255) NOT NULL
 ); 
 
-DROP TABLE IF EXISTS maintenance_events;
-CREATE TABLE maintenance_events (
+DROP TABLE IF EXISTS project1.maintenance_events CASCADE;
+CREATE TABLE project1.maintenance_events (
     maintenance_id SERIAL PRIMARY KEY,
     aircraft_registration_number INT NOT NULL,
     maintenance_starttime TIMESTAMP NOT NULL,
@@ -129,17 +143,17 @@ CREATE TABLE maintenance_events (
     subsystem_id INT NOT NULL,
     maintenance_type_id INT NOT NULL,
     CONSTRAINT fk_aircraft_registration_number
-        FOREIGN KEY (aircraft_registration_number) REFERENCES aircrafts (aircraft_registration_number),
+        FOREIGN KEY (aircraft_registration_number) REFERENCES project1.aircrafts (aircraft_registration_number),
     CONSTRAINT fk_airport_id
-        FOREIGN KEY (airport_id) REFERENCES airports (airport_id),
+        FOREIGN KEY (airport_id) REFERENCES project1.airports (airport_id),
     CONSTRAINT fk_subsystem_id
-        FOREIGN KEY (subsystem_id) REFERENCES subsystems (subsystem_id),
+        FOREIGN KEY (subsystem_id) REFERENCES project1.subsystems (subsystem_id),
     CONSTRAINT fk_maintenance_type_id
-        FOREIGN KEY (maintenance_type_id) REFERENCES maintenance_types (maintenance_type_id)
+        FOREIGN KEY (maintenance_type_id) REFERENCES project1.maintenance_types (maintenance_type_id)
 ); 
 
-DROP TABLE IF EXISTS aircraft_slots;
-CREATE TABLE aircraft_slots (
+DROP TABLE IF EXISTS project1.aircraft_slots CASCADE;
+CREATE TABLE project1.aircraft_slots (
     aircraft_slot_id SERIAL PRIMARY KEY,
     aircraft_registration_number INT NOT NULL,
     slot_start TIMESTAMP NOT NULL,
@@ -148,20 +162,20 @@ CREATE TABLE aircraft_slots (
     slot_scheduled BOOLEAN NOT NULL,
     maintenance_id INT NOT NULL,
     CONSTRAINT fk_aircraft_registration_number
-        FOREIGN KEY (aircraft_registration_number) REFERENCES aircrafts (aircraft_registration_number),
+        FOREIGN KEY (aircraft_registration_number) REFERENCES project1.aircrafts (aircraft_registration_number),
     CONSTRAINT fk_maintenance_id
-        FOREIGN KEY (maintenance_id) REFERENCES maintenance_events (maintenance_id)
+        FOREIGN KEY (maintenance_id) REFERENCES project1.maintenance_events (maintenance_id)
 ); 
 
-DROP TABLE IF EXISTS reporteurs;
-CREATE TABLE reporteurs (
+DROP TABLE IF EXISTS project1.reporteurs CASCADE;
+CREATE TABLE project1.reporteurs (
     reporteur_id SERIAL PRIMARY KEY,
     reporteur_class varchar(255) NOT NULL,
     reporteur_name varchar(255) NOT NULL
 ); 
 
-DROP TABLE IF EXISTS work_orders;
-CREATE TABLE work_orders (
+DROP TABLE IF EXISTS project1.work_orders CASCADE;
+CREATE TABLE project1.work_orders (
     work_order_id SERIAL PRIMARY KEY,
     aircraft_registration_number INT NOT NULL,
     maintenance_id INT NOT NULL,
@@ -175,11 +189,11 @@ CREATE TABLE work_orders (
     due_date TIMESTAMP NOT NULL,
     reporting_date TIMESTAMP NOT NULL,
     CONSTRAINT fk_aircraft_registration_number
-        FOREIGN KEY (aircraft_registration_number) REFERENCES aircrafts (aircraft_registration_number),
+        FOREIGN KEY (aircraft_registration_number) REFERENCES project1.aircrafts (aircraft_registration_number),
     CONSTRAINT fk_maintenance_id
-        FOREIGN KEY (maintenance_id) REFERENCES maintenance_events (maintenance_id),
+        FOREIGN KEY (maintenance_id) REFERENCES project1.maintenance_events (maintenance_id),
     CONSTRAINT fk_airport_id
-        FOREIGN KEY (airport_id) REFERENCES airports (airport_id),
+        FOREIGN KEY (airport_id) REFERENCES project1.airports (airport_id),
     CONSTRAINT fk_reporteur_id
-        FOREIGN KEY (reporteur_id) REFERENCES reporteurs (reporteur_id)
+        FOREIGN KEY (reporteur_id) REFERENCES project1.reporteurs (reporteur_id)
 ); 
