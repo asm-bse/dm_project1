85d82b7001035c6be38b4856d0793bb428d51b4b
Alex molin <alexmolin@Alexs-MacBook-Pro.local>
2024-10-28 15:27:19 +0100

    added comments

--- ddl.sql
+++ ddl.sql
@@ -2,8 +2,8 @@
 DROP TABLE IF EXISTS project1.customers CASCADE;
 CREATE TABLE project1.customers 
 (
-    customer_id SERIAL PRIMARY KEY,
-    name varchar(255) NOT NULL,
+    customer_id SERIAL PRIMARY KEY, --we are not storing passwords. OTP or other third-party services can be used.
+    name varchar(255) NOT NULL, -- Name and Family name are stored together, could be split if needed
     email varchar(255) NOT NULL,
     phone_number varchar(255) NOT NULL,
     address varchar(255) NOT NULL
@@ -11,7 +11,7 @@ CREATE TABLE project1.customers
 
 DROP TABLE IF EXISTS project1.aircrafts CASCADE;
 CREATE TABLE project1.aircrafts (
-    aircraft_registration_number SERIAL PRIMARY KEY,
+    aircraft_registration_number SERIAL PRIMARY KEY, --unique, analog to car's plate
     aircraft_type varchar(255) NOT NULL,
     aircraft_company varchar(255) NOT NULL,
     aircraft_capacity int NOT NULL
@@ -19,15 +19,15 @@ CREATE TABLE project1.aircrafts (
 
 DROP TABLE IF EXISTS project1.airports CASCADE;
 CREATE TABLE project1.airports (
-    airport_id varchar(3) NOT NULL PRIMARY KEY,
-    airport_name varchar(255) NOT NULL,
+    airport_id varchar(3) NOT NULL PRIMARY KEY, --unique, like LAX or JFK
+    airport_name varchar(255) NOT NULL, 
     airport_city varchar(255) NOT NULL,
     airport_country varchar(255) NOT NULL
 );   
 
 DROP TABLE IF EXISTS project1.flights CASCADE;
 CREATE TABLE project1.flights (
-    flight_number varchar(6) NOT NULL PRIMARY KEY,
+    flight_number varchar(6) NOT NULL PRIMARY KEY, --unique, mask AA9999, one flight_nubmer is used by multiple scheduler flights, like daily
     origin varchar(3) NOT NULL,
     destination varchar(3) NOT NULL,
     airline_id INT NOT NULL,
@@ -60,15 +60,15 @@ CREATE TABLE project1.flight_statuses (
 
 DROP TABLE IF EXISTS project1.flight_data CASCADE;
 CREATE TABLE project1.flight_data (
-    flight_id SERIAL PRIMARY KEY,
+    flight_id SERIAL PRIMARY KEY, -- unique for each flight any aircraft does
     flight_number varchar(6) NOT NULL,
     aircraft_registration_number INT NOT NULL,
     flight_status_id INT NOT NULL,
     problem_id INT NOT NULL,
-    number_of_passengers int NOT NULL,
+    number_of_passengers int NOT NULL, --could be counted from seats/bookings in next iteration
     number_of_cabin_crew int NOT NULL,
     number_of_flight_crew int NOT NULL,
-    available_seating int NOT NULL,
+    available_seating int NOT NULL, --could be counted from seats/bookings in next iteration
     scheduled_departure_date DATE NOT NULL,
     scheduled_departure_time TIME NOT NULL,
     CONSTRAINT fk_flight_number
@@ -85,8 +85,8 @@ CREATE TABLE project1.flight_data (
 DROP TABLE IF EXISTS project1.seat_classes CASCADE;
 CREATE TABLE project1.seat_classes 
 (
-    seat_number varchar(3) NOT NULL PRIMARY KEY, --1a to 40f
-    seat_class varchar(33) NOT NULL, --rows 1 - 3 are business and rows 4 - 40 are economy
+    seat_number varchar(3) NOT NULL PRIMARY KEY, --1a to 40f, considering that all the aircrafts have the same seatings
+    seat_class varchar(33) NOT NULL, --rows 1 - 3 are business and rows 4 - 40 are economy for all the aircrafts
     CONSTRAINT fk_seat_number
         FOREIGN KEY (seat_number) REFERENCES project1.seat_classes (seat_number)
 );
@@ -97,7 +97,7 @@ CREATE TABLE project1.seats
 (
     seat_id SERIAL PRIMARY KEY,
     seat_number varchar(3) NOT NULL, -- foreign key
-    seat_status varchar(33) NOT NULL, --available or occupied
+    seat_status varchar(33) NOT NULL, --available or occupied, could be pulled from bookings in next iteration
     flight_id INT NOT null, -- foreign_key
     CONSTRAINT fk_flight_id
         FOREIGN KEY (flight_id) REFERENCES project1.flight_data (flight_id),
@@ -107,8 +107,8 @@ CREATE TABLE project1.seats
 
 DROP TABLE IF EXISTS project1.bookings CASCADE;
 CREATE TABLE project1.bookings (
-    booking_id SERIAL PRIMARY KEY,
-    flight_id INT NOT NULL,
+    booking_id SERIAL PRIMARY KEY, --used as confirmation code as it it unique. Now we a restricted to 1 seat per booking.
+    flight_id INT NOT NULL, --might be removed from this table as thin information can be pulled from seat_id, but for ow we prefer to keep straing connection with flight_data
     customer_id INT NOT NULL,
     seat_id INT NOT NULL,
     price NUMERIC(7,2) NOT NULL,
@@ -139,9 +139,9 @@ CREATE TABLE project1.maintenance_events (
     aircraft_registration_number INT NOT NULL,
     maintenance_starttime TIMESTAMP NOT NULL,
     duration INTERVAL NOT NULL,
-    airport_id VARCHAR(3) NOT NULL,
-    subsystem_id INT NOT NULL,
-    maintenance_type_id INT NOT NULL,
+    airport_id VARCHAR(3) NOT NULL, --where the maintenance is taking place
+    subsystem_id INT NOT NULL, --which subsystem is being maintained
+    maintenance_type_id INT NOT NULL, 
     CONSTRAINT fk_aircraft_registration_number
         FOREIGN KEY (aircraft_registration_number) REFERENCES project1.aircrafts (aircraft_registration_number),
     CONSTRAINT fk_airport_id
@@ -160,7 +160,7 @@ CREATE TABLE project1.aircraft_slots (
     slot_end TIMESTAMP NOT NULL,
     slot_type VARCHAR(255) NOT NULL,
     slot_scheduled BOOLEAN NOT NULL,
-    maintenance_id INT NOT NULL,
+    maintenance_id INT NOT NULL, 
     CONSTRAINT fk_aircraft_registration_number
         FOREIGN KEY (aircraft_registration_number) REFERENCES project1.aircrafts (aircraft_registration_number),
     CONSTRAINT fk_maintenance_id
@@ -169,7 +169,7 @@ CREATE TABLE project1.aircraft_slots (
 
 DROP TABLE IF EXISTS project1.reporteurs CASCADE;
 CREATE TABLE project1.reporteurs (
-    reporteur_id SERIAL PRIMARY KEY,
+    reporteur_id SERIAL PRIMARY KEY, -- can be replaced with worker_id if we will store tables with all the staff of all the airports and aircompanies
     reporteur_class varchar(255) NOT NULL,
     reporteur_name varchar(255) NOT NULL
 ); 
@@ -187,7 +187,7 @@ CREATE TABLE project1.work_orders (
     frequency INT NOT NULL,
     reporteur_id INT NOT NULL,
     due_date TIMESTAMP NOT NULL,
-    reporting_date TIMESTAMP NOT NULL,
+    reporting_date TIMESTAMP NOT NULL, --worker_id can be added as a foreign key if we will store tables with all the staff of all the airports and aircompanies
     CONSTRAINT fk_aircraft_registration_number
         FOREIGN KEY (aircraft_registration_number) REFERENCES project1.aircrafts (aircraft_registration_number),
     CONSTRAINT fk_maintenance_id
